112063e37abe57aabcaa49cc26ec0feaa79361dc
= <konradbjk@gmail.com>
2022-05-01 14:14:21 +0200

    adding rest

---
+++ components/TextHeader.jsx
@@ -0,0 +1,7 @@
+const TextHeader = ({text}) => (
+    <h1 className='text-4xl'>
+    {text}
+  </h1>
+)
+
+export default TextHeader;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+
+import "@/styles/global.css";
+
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+import "regenerator-runtime/runtime";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    // Ropsten 3
+    // Rinkeby 4
+    // Kovan 42
+  const supportedChainIds = [3, 4, 42];
+
+  const connectors = {
+    injected: {},
+  };
+
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
